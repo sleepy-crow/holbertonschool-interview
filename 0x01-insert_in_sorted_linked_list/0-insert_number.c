e3ffde64d14a3c036849acf14d2771cc8f52c2fc
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-16 18:21:25 -0500

    added description of the function

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -13,35 +13,45 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 
-	listint_t *neo, *cur = *head;
-
-	neo = malloc(sizeof(listint_t));
+	listint_t *neo = malloc(sizeof(listint_t));
+	listint_t *cur;
 
 	if (neo == NULL)
 		return (NULL);
-
 	neo->n = number;
-	if (neo->n < cur->n)
+	if (head == NULL)
+	{
+		neo = add_nodeint_end(head, number);
+		*head = neo;
+		return (neo);
+	}
+	cur = *head;
+	if (cur == NULL)
+	{
+		neo->next = NULL;
+		return (neo);
+	}
+	if (cur->n > number)
 	{
 		neo->next = cur;
 		*head = neo;
+		return (neo);
 	}
+
+	while (cur->next->n < number && cur->next->next != NULL)
+	{
+		cur = cur->next;
+	}
+	if (cur->next !=  NULL)
+		cur = cur->next;
+	if (cur->n < number)
+		add_nodeint_end(head, number);
 	else
 	{
-		while (1)
-		{
-			if (neo->n < cur->next->n || cur->next == NULL)
-			{
-				neo->next = cur->next;
-				cur->next = neo;
-				break;
-			}
-			else
-			{
-				cur = cur->next;
-			}
-		}
+		neo->next = cur->next;
+		cur->next = neo;
 	}
+
 	return (neo);
 }
 
