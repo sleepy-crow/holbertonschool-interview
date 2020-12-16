d763ac5df189e73b3a5863320cb908e6832ff209
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-16 16:22:22 -0500

    adding node to a single listed list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    
+    listint_t *neo, *cur = *head;
+
+    neo = malloc(sizeof(listint_t));
+
+    if (neo == NULL)
+        return (NULL);
+
+    neo->n = number;
+    if (neo->n < cur->n)
+    {
+        neo->next = cur;
+        *head = neo;
+    }
+    else
+    {
+        while (1)
+        {
+            if (neo->n < cur->next->n || cur->next == NULL)
+            {
+                neo->next = cur->next;
+                cur->next = neo;
+                break;
+            }
+            else
+            {
+                cur = cur->next;
+            }
+        }
+    }
+    return (neo);
+}
