91a2c10e43d09d93299f30706b490a6a902238c0
noah172 <jsebastian.garcia1995@gmail.com>
2021-04-15 14:48:50 -0500

    make it rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+
+"""
+def rain(walls):
+    if len(walls) < 3 or len(walls) == 0:
+        return 0
+
+    if len(walls) == 3 and walls[-1] == 0:
+        return 0
+
+    water = 0
+    wall1 = 0
+    wall2 = 0
+    pos = 0
+    for i in walls:
+        pos += 1
+        if pos == len(walls):
+            break
+        if i > 0 and wall1 == 0:
+            wall1 = i
+        if wall1 > 0 and i > 0:
+            wall2 = i
+            if wall1 > wall2:
+                water += wall2
+            else:
+                water += wall1
+            wall1 = 0
+            wall2 = 0
+
+    return water
