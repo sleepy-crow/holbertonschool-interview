302453bd47601bb94ad260b95929bc2a9121f813
noah172 <jsebastian.garcia1995@gmail.com>
2021-06-02 17:32:14 -0500

    now the node returned is where the loop begins

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -1,17 +1,15 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - function that finds the loop in a linked list.
- * 
+ * is_a_loop_there - function that checks if there is a loop
+ * in the list.
+ *
  * @head: the head of the list to examinate.
- * 
- * Return: the node where the loop is or NULL if there is not lopp.
+ *
+ * Return: 0 if there is no loop and 1 if there is a loop
  */
-listint_t *find_listint_loop(listint_t *head)
+int is_a_loop_there(listint_t *head)
 {
-	if (head == NULL || head->next == NULL)
-		return (NULL);
-
 	listint_t *turtle, *hare;
 
 	turtle = head;
@@ -22,9 +20,67 @@ listint_t *find_listint_loop(listint_t *head)
 		turtle = turtle->next;
 		hare = hare->next->next;
 		if (hare == NULL || hare->next == NULL)
-			return (NULL);
+			return (0);
 		if (hare == turtle)
-			return (hare);
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * some_where_over_the_rainbow - function that finds
+ * the node where the loop starts.
+ *
+ * @head: looped linked list.
+ *
+ * Return: the node where the loop begins
+ */
+listint_t *some_where_over_the_rainbow(listint_t *head)
+{
+	listint_t *turtle, *hare;
+
+	turtle = head;
+	hare = head;
+
+	while (hare != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+		if (hare == turtle)
+		{
+			turtle = head;
+			while (1)
+			{
+				turtle = turtle->next;
+				hare = hare->next;
+				if (hare == turtle)
+					return (turtle);
+			}
+		}
+	}
+	return (NULL);
+}
+/**
+ * find_listint_loop - function that finds the loop in a linked list.
+ *
+ * @head: the head of the list to examinate.
+ *
+ * Return: the node where the loop is or NULL if there is not loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	int looper = is_a_loop_there(head);
+
+	if (looper == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (some_where_over_the_rainbow(head));
 	}
 	return (NULL);
 }
