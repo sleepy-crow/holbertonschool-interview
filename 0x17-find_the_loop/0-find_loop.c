2db4204b3e2d25f0366100ded09f2b1352991063
noah172 <jsebastian.garcia1995@gmail.com>
2021-06-03 21:37:14 -0500

    modified method

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -3,66 +3,6 @@
 #include <stdlib.h>
 
 /**
- * is_a_loop_there - function that checks if there is a loop
- * in the list.
- *
- * @head: the head of the list to examinate.
- *
- * Return: 0 if there is no loop and 1 if there is a loop
- */
-int is_a_loop_there(listint_t *head)
-{
-	listint_t *turtle, *hare;
-
-	turtle = head;
-	hare = head;
-
-	while (hare != NULL)
-	{
-		turtle = turtle->next;
-		hare = hare->next->next;
-		if (hare == NULL || hare->next == NULL)
-			return (0);
-		if (hare == turtle)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * some_where_over_the_rainbow - function that finds
- * the node where the loop starts.
- *
- * @head: looped linked list.
- *
- * Return: the node where the loop begins
- */
-listint_t *some_where_over_the_rainbow(listint_t *head)
-{
-	listint_t *turtle, *hare;
-
-	turtle = head;
-	hare = head;
-
-	while (hare != NULL)
-	{
-		turtle = turtle->next;
-		hare = hare->next->next;
-		if (hare == turtle)
-		{
-			turtle = head;
-			while (1)
-			{
-				turtle = turtle->next;
-				hare = hare->next;
-				if (hare == turtle)
-					return (turtle);
-			}
-		}
-	}
-	return (NULL);
-}
-/**
  * find_listint_loop - function that finds the loop in a linked list.
  *
  * @head: the head of the list to examinate.
@@ -71,18 +11,27 @@ listint_t *some_where_over_the_rainbow(listint_t *head)
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	if (head == NULL || head->next == NULL)
+	listint_t *hare, *turtle;
+
+	if (head == NULL)
 		return (NULL);
 
-	int looper = is_a_loop_there(head);
+	turtle = head->next;
+	hare = turtle->next;
 
-	if (looper == 0)
+	while (turtle && hare && hare->next)
 	{
-		return (NULL);
-	}
-	else
-	{
-		return (some_where_over_the_rainbow(head));
+		if (turtle == head || hare == head)
+			return (head);
+
+		if (hare == turtle && hare != head)
+		{
+			head = head->next;
+			hare = head;
+			turtle = head;
+		}
+		turtle = turtle->next;
+		hare = hare->next->next;
 	}
 	return (NULL);
 }
