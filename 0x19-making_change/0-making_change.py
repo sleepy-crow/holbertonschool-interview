f0fbc351fe01153e668bc3bbb3af9946920ed66b
noah172 <jsebastian.garcia1995@gmail.com>
2021-06-24 19:37:06 -0500

    making change

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Making Change
+"""
+import math
+
+def makeChange(coins, total):
+    """
+    function that makes change
+    """
+
+    if (total <= 0):
+        return 0
+
+    if (len(coins) == 0):
+        return -1
+
+    coins.sort()
+
+    change = 0
+    length = len(coins) - 1
+    left = total
+    subtotalC = 0
+
+    while (length > 0):
+        change += math.floor(left / coins[length])
+        currentC = math.floor(left / coins[length])
+        subtotalC += coins[length] * currentC
+        left = left % coins[length]
+        length -= 1
+
+
+    if (subtotalC != total):
+        return -1
+    
+    return change
