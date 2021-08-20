93436ff7d87e110df8ac632c34f0a179e24064b1
noah172 <jsebastian.garcia1995@gmail.com>
2021-08-19 19:33:33 -0500

    sub-string

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,11 @@
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
+
+/* libraries */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* prototypes */
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif
